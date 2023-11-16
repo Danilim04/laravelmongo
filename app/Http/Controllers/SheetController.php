f4499d0e14394585cf7eee4610aa4eb213ceec95
Daniel Ferraz <daniel.ferraz04@hotmail.com>
2023-11-16 20:49:45 -0300

    biblioteca-Google Sheets

--- app/Http/Controllers/SheetController.php
+++ app/Http/Controllers/SheetController.php
@@ -4,12 +4,32 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Doc;
+use Google\Client;
+use Google\Service\Sheets;
+
+
 
 
 class SheetController extends Controller
 {
-    public function ApiSheets (Request $request) {
-            $docs = Doc::where('ar', '55231066438011000166005338304')->get();
-            dd($docs);
+    public function ApiSheets(Request $request)
+    {
+        set_time_limit(3600);
+        $client = new Client();
+        $client->setApplicationName('nome da sua aplicação');
+        $client->setScopes([Sheets::SPREADSHEETS]);
+        $client->setAccessType('offline');
+        $client->setAuthConfig(__DIR__ . '/credencias.json');
+        $client->setPrompt('select_account consent');
+
+        $service = new Sheets($client);
+        
+        $spreadsheetId = "17DBBTG49P-7h9xJjwlojxCb6bL02WubbWzRx8AqVeOQ";
+
+        $range = "A2:U";
+        $response = $service->spreadsheets_values->get($spreadsheetId, $range);
+        $values = $response->getValues();
+
+        dd(json_encode($values));
     }
 }
