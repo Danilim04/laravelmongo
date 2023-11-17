5fe07e83312bbf6faa7354b2ad8b5e5ce26b92ae
Daniel Alves Ferraz <daniel.ferraz04@hotmail.com>
2023-11-17 00:01:48 -0300

    adicionando conteudo planilha

--- app/Http/Controllers/SheetController.php
+++ app/Http/Controllers/SheetController.php
@@ -3,18 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\Doc;
 use Google\Client;
 use Google\Service\Sheets;
-
-
-
+use \Google\Service\Sheets\ValueRange;
 
 class SheetController extends Controller
 {
     public function ApiSheets(Request $request)
     {
-        set_time_limit(3600);
         $client = new Client();
         $client->setApplicationName('nome da sua aplicação');
         $client->setScopes([Sheets::SPREADSHEETS]);
@@ -23,13 +19,35 @@ class SheetController extends Controller
         $client->setPrompt('select_account consent');
 
         $service = new Sheets($client);
-        
-        $spreadsheetId = "17DBBTG49P-7h9xJjwlojxCb6bL02WubbWzRx8AqVeOQ";
 
+        $spreadsheetId = "17DBBTG49P-7h9xJjwlojxCb6bL02WubbWzRx8AqVeOQ";
+      
         $range = "A2:U";
         $response = $service->spreadsheets_values->get($spreadsheetId, $range);
         $values = $response->getValues();
+        
+        
+        $newValues = [
+            
+            ["Novo Valor 1", "Novo Valor 2", "Novo Valor 3"],
+        ];
 
-        dd(json_encode($values));
+        $body = new ValueRange([
+            'values' => $newValues
+        ]);
+
+        $rangeToWrite = "A" . (count($values) + 2) . ":U"; // Adiciona após a última linha existente
+
+        $params = [
+            'valueInputOption' => 'RAW'
+        ];
+
+        $result = $service->spreadsheets_values->append($spreadsheetId, $rangeToWrite, $body, $params);
+
+        if ($result->getUpdates()->getUpdatedCells() > 0) {
+            dd('Valores adicionados com sucesso!');
+        } else {
+            dd('Falha ao adicionar valores.');
+        }
     }
 }
